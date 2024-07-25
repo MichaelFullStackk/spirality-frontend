61338bdd01245e6487262408b8766759dd6f507f
Админ <kustanaev.tamirlan@gmail.com>
2024-07-25 17:14:59 +0600

    hotfix (pls kill me 2)

--- src/app/components/layout/PathNode.js
+++ src/app/components/layout/PathNode.js
@@ -14,7 +14,7 @@ const PathNode = ({ topic_id, course_id, isLast, index }) => {
   useEffect(() => {
     const fetchTopicData = async () => {
       try {
-        const response = await axios.get(`https://spirality-frontend.vercel.app/api/course/${course_id}`);
+        const response = await axios.get(`https://spirality-backend-production.up.railway.app/api/course/${course_id}`);
         const courseData = response.data;
         const topic = courseData.topics.find(t => t._id === topic_id);
         
@@ -33,7 +33,7 @@ const PathNode = ({ topic_id, course_id, isLast, index }) => {
 
   const handleClick = () => {
     if (isAvailable) {
-      router.push(`https://spirality-backend-production.up.railway.app/pages/lesson/course-${course_id}&lesson-${topic_id}`);
+      router.push(`https://spirality-frontend.vercel.app/pages/lesson/course-${course_id}&lesson-${topic_id}`);
     }
   };
 
